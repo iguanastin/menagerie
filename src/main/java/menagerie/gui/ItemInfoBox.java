4a8d848d1e3f6bb19ac995f3bcf005450391473d
iguanastin <austinthompson194@gmail.com>
2019-03-03 13:51:17 -0700

    Added more info to ItemInfoBox, made it collapsible

--- src/main/java/menagerie/gui/ItemInfoBox.java
+++ src/main/java/menagerie/gui/ItemInfoBox.java
@@ -2,50 +2,87 @@ package menagerie.gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import menagerie.model.menagerie.ImageInfo;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class ItemInfoBox extends VBox {
 
-    private Label fileSize, resolution, filePath;
-
+    private static final String DEFAULT_ID_TEXT = "ID: N/A";
+    private static final String DEFAULT_DATE_TEXT = "N/A";
     private static final String DEFAULT_FILESIZE_TEXT = "0B";
     private static final String DEFAULT_RESOLUTION_TEXT = "0x0";
     private static final String DEFAULT_FILEPATH_TEXT = "N/A";
+
+    private Label idLabel = new Label(DEFAULT_ID_TEXT);
+    private Label dateLabel = new Label(DEFAULT_DATE_TEXT);
+    private Label fileSizeLabel = new Label(DEFAULT_FILESIZE_TEXT);
+    private Label resolutionLabel = new Label(DEFAULT_RESOLUTION_TEXT);
+    private Label filePathLabel = new Label(DEFAULT_FILEPATH_TEXT);
+
+    private boolean extended = false;
 
 
     public ItemInfoBox() {
         setPadding(new Insets(5));
         setStyle("-fx-background-color: -fx-base;");
-        setSpacing(5);
+        setSpacing(2);
 
-        fileSize = new Label(DEFAULT_FILESIZE_TEXT);
-        resolution = new Label(DEFAULT_RESOLUTION_TEXT);
-        filePath = new Label(DEFAULT_FILEPATH_TEXT);
-        getChildren().addAll(fileSize, resolution, filePath);
+        addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            setExtended(!isExtended());
+        });
+
+        getChildren().addAll(resolutionLabel, fileSizeLabel);
+    }
+
+    public boolean isExtended() {
+        return extended;
+    }
+
+    public void setExtended(boolean b) {
+        if (b == extended) return;
+
+        extended = b;
+        if (extended) {
+            getChildren().addAll(idLabel, dateLabel, filePathLabel);
+            System.out.println(1);
+        } else {
+            getChildren().removeAll(idLabel, dateLabel, filePathLabel);
+        }
     }
 
     public void setItem(ImageInfo item) {
         if (item != null) {
-            fileSize.setText(bytesToPrettyString(item.getFile().length()));
-            filePath.setText(item.getFile().toString());
+            idLabel.setText("ID: " + item.getId());
+            dateLabel.setText(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(Locale.getDefault()).withZone(ZoneId.systemDefault()).format(new Date(item.getDateAdded()).toInstant()));
+            fileSizeLabel.setText(bytesToPrettyString(item.getFile().length()));
+            filePathLabel.setText(item.getFile().toString());
             if (item.isImage()) { //TODO: Support for video resolution
                 if (item.getImage().isBackgroundLoading() && item.getImage().getProgress() != 1) {
-                    resolution.setText("Loading...");
+                    resolutionLabel.setText("Loading...");
                     item.getImage().progressProperty().addListener((observable, oldValue, newValue) -> {
                         if (newValue.doubleValue() == 1 && !item.getImage().isError())
-                            resolution.setText((int) item.getImage().getWidth() + "x" + (int) item.getImage().getHeight());
+                            resolutionLabel.setText((int) item.getImage().getWidth() + "x" + (int) item.getImage().getHeight());
                     });
                 } else {
-                    resolution.setText((int) item.getImage().getWidth() + "x" + (int) item.getImage().getHeight());
+                    resolutionLabel.setText((int) item.getImage().getWidth() + "x" + (int) item.getImage().getHeight());
                 }
             } else {
-                resolution.setText(DEFAULT_RESOLUTION_TEXT);
+                resolutionLabel.setText(DEFAULT_RESOLUTION_TEXT);
             }
         } else {
-            fileSize.setText(DEFAULT_FILESIZE_TEXT);
-            resolution.setText(DEFAULT_RESOLUTION_TEXT);
-            filePath.setText(DEFAULT_FILEPATH_TEXT);
+            idLabel.setText(DEFAULT_ID_TEXT);
+            dateLabel.setText(DEFAULT_DATE_TEXT);
+            fileSizeLabel.setText(DEFAULT_FILESIZE_TEXT);
+            resolutionLabel.setText(DEFAULT_RESOLUTION_TEXT);
+            filePathLabel.setText(DEFAULT_FILEPATH_TEXT);
         }
     }
 
