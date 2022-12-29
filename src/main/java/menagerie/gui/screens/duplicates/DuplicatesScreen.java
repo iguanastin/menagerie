a6625d1a4f676b11b01f7f97955519661b2c5b5f
Julian <hi@example.com>
2022-12-29 20:20:13 +0100

    streamline methods of DuplicatesScreen

--- src/main/java/menagerie/gui/screens/duplicates/DuplicatesScreen.java
+++ src/main/java/menagerie/gui/screens/duplicates/DuplicatesScreen.java
@@ -83,7 +83,8 @@ public class DuplicatesScreen extends Screen {
   private final BooleanProperty deleteFile = new SimpleBooleanProperty(true);
   private final BooleanProperty preload = new SimpleBooleanProperty(true);
   // TODO. remove?
-  private Image preloadPrevLeft = null, preloadPrevRight = null, preloadNextLeft = null, preloadNextRight = null;
+  private Image preloadPrevLeft = null, preloadPrevRight = null, preloadNextLeft = null,
+      preloadNextRight = null;
 
   private ObjectListener<Item> selectListener = null;
   private final ListChangeListener<Tag> leftTagListener = c -> {
@@ -98,6 +99,178 @@ public class DuplicatesScreen extends Screen {
   };
 
   public DuplicatesScreen() {
+    registerKeyEvents();
+    getStyleClass().addAll(ROOT_STYLE_CLASS);
+
+    // ---------------------------------------------- Center Element -----------------------------------------------
+    registerContextMenuRequestedEvent(leftMediaView, "Add tags to other -->",
+        currentPair.getObject1(), currentPair.getObject2());
+    registerContextMenuRequestedEvent(rightMediaView, "<-- Add tags to other",
+        currentPair.getObject2(), currentPair.getObject1());
+    registerCellFactory(leftTagList, currentPair.getObject2(), currentPair.getObject1());
+    registerCellFactory(rightTagList, currentPair.getObject1(), currentPair.getObject2());
+    configureCenterElements();
+    addSplitPane();
+
+    // ---------------------------------------- Bottom element -----------------------------------------------------
+    VBox bottom = new VBox(5);
+    bottom.setPadding(new Insets(5));
+    constructFirstElement(bottom);
+    constructSecondElement(bottom);
+  }
+
+  private void constructSecondElement(VBox bottom) {
+    Button prevPairButton = new Button("<-");
+    prevPairButton.setOnAction(event -> previewPrev());
+    Button closeButton = new Button("Close");
+    closeButton.setOnAction(event -> close());
+    Button nextPairButton = new Button("->");
+    nextPairButton.setOnAction(event -> previewNext());
+    registerAddListenerToCheckbox();
+    HBox hb = new HBox(5, prevPairButton, closeButton, nextPairButton, nonDupeCheckBox);
+    hb.setAlignment(Pos.CENTER);
+    bottom.getChildren().add(hb);
+    setBottom(bottom);
+    setDefaultFocusNode(closeButton);
+  }
+
+  private void registerAddListenerToCheckbox() {
+    nonDupeCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue) {
+        menagerie.addNonDuplicate(currentPair);
+      } else {
+        menagerie.removeNonDuplicate(currentPair);
+      }
+    });
+  }
+
+  private void constructFirstElement(VBox bottom) {
+    configureIndexTextField();
+    HBox hbc = new HBox(indexTextField, similarityLabel);
+    hbc.setAlignment(Pos.CENTER);
+    Button leftDeleteButton = new Button("Delete");
+    leftDeleteButton.setOnAction(event -> deleteItem(currentPair.getObject1()));
+    Button rightDeleteButton = new Button("Delete");
+    rightDeleteButton.setOnAction(event -> deleteItem(currentPair.getObject2()));
+    HBox hbl = new HBox(leftDeleteButton);
+    hbl.setAlignment(Pos.CENTER_LEFT);
+    HBox hbr = new HBox(rightDeleteButton);
+    hbr.setAlignment(Pos.CENTER_RIGHT);
+    bottom.getChildren().add(new BorderPane(hbc, null, hbr, null, hbl));
+  }
+
+  private void configureIndexTextField() {
+    indexTextField.setAlignment(Pos.CENTER_RIGHT);
+    indexTextField.setPrefWidth(50);
+    indexTextField.setOnAction(event -> {
+      int i = pairs.indexOf(currentPair);
+      try {
+        int temp = Integer.parseInt(indexTextField.getText()) - 1;
+        i = Math.max(0, Math.min(temp, pairs.size() - 1)); // Clamp to valid indices
+      } catch (NumberFormatException e) {
+        // Nothing
+      }
+
+      preview(pairs.get(i));
+      requestFocus();
+    });
+  }
+
+  private void addSplitPane() {
+    BorderPane lbp = new BorderPane(null, null, leftTagList, null, leftInfoBox);
+    lbp.setPickOnBounds(false);
+    BorderPane rbp = new BorderPane(null, null, rightInfoBox, null, rightTagList);
+    rbp.setPickOnBounds(false);
+    SplitPane sp =
+        new SplitPane(new StackPane(leftMediaView, lbp), new StackPane(rightMediaView, rbp));
+    sp.setOnMouseEntered(event -> {
+      lbp.setRight(leftTagList);
+      rbp.setLeft(rightTagList);
+    });
+    sp.setOnMouseExited(event -> {
+      lbp.setRight(null);
+      rbp.setLeft(null);
+    });
+    setCenter(sp);
+  }
+
+  private void configureCenterElements() {
+    leftTagList.setPrefWidth(200);
+    rightTagList.setPrefWidth(200);
+    leftInfoBox.setAlignment(Pos.BOTTOM_LEFT);
+    leftInfoBox.setMaxHeight(USE_PREF_SIZE);
+    leftInfoBox.setOpacity(0.75);
+    BorderPane.setAlignment(leftInfoBox, Pos.BOTTOM_LEFT);
+    rightInfoBox.setAlignment(Pos.BOTTOM_RIGHT);
+    rightInfoBox.setMaxHeight(USE_PREF_SIZE);
+    rightInfoBox.setOpacity(0.75);
+    BorderPane.setAlignment(rightInfoBox, Pos.BOTTOM_RIGHT);
+  }
+
+  private void registerCellFactory(ListView<Tag> tagListView, MediaItem obj1, MediaItem obj2) {
+    tagListView.setCellFactory(param -> {
+      // TODO
+      TagListCell c = new TagListCell(null, null) {
+        private final PseudoClass otherHasPseudoClass = PseudoClass.getPseudoClass("other-missing");
+
+        private final BooleanProperty otherMissing = new BooleanPropertyBase() {
+          @Override
+          protected void invalidated() {
+            pseudoClassStateChanged(otherHasPseudoClass, get());
+          }
+
+          @Override
+          public Object getBean() {
+            return this;
+          }
+
+          @Override
+          public String getName() {
+            return "otherMissing";
+          }
+        };
+
+        @Override
+        protected void updateItem(Tag tag, boolean empty) {
+          super.updateItem(tag, empty);
+
+          otherMissing.set(tag != null && !obj1.hasTag(tag));
+        }
+      };
+
+      MenuItem addToOther = new MenuItem("Add to other");
+      addToOther.setOnAction(event -> obj1.addTag(c.getItem()));
+      MenuItem removeTag = new MenuItem("Remove tag");
+      removeTag.setOnAction(event -> obj2.removeTag(c.getItem()));
+      ContextMenu cm = new ContextMenu(addToOther, new SeparatorMenuItem(), removeTag);
+      c.setOnContextMenuRequested(
+          event -> cm.show(c.getScene().getWindow(), event.getScreenX(), event.getScreenY()));
+      return c;
+    });
+  }
+
+  private void registerContextMenuRequestedEvent(DynamicMediaView mediaView, String menuItemLabel,
+                                                 MediaItem obj1, MediaItem obj2) {
+    mediaView.setOnContextMenuRequested(event -> {
+      MenuItem select = new MenuItem("Select in explorer");
+      MenuItem combineTags = new MenuItem(menuItemLabel);
+      ContextMenu cm = new ContextMenu(select, combineTags);
+      select.setOnAction(event1 -> {
+        if (selectListener != null) {
+          selectListener.pass(obj1);
+        }
+        cm.hide();
+        close();
+      });
+      combineTags.setOnAction(event1 -> {
+        obj1.getTags().forEach(obj2::addTag);
+        cm.hide();
+      });
+      cm.show(mediaView.getScene().getWindow(), event.getScreenX(), event.getScreenY());
+    });
+  }
+
+  private void registerKeyEvents() {
     addEventHandler(KeyEvent.KEY_PRESSED, event -> {
       if (event.getCode() == KeyCode.ESCAPE) {
         close();
@@ -116,198 +289,6 @@ public class DuplicatesScreen extends Screen {
         event.consume();
       }
     });
-
-    getStyleClass().addAll(ROOT_STYLE_CLASS);
-
-    // ---------------------------------------------- Center Element -----------------------------------------------
-    leftMediaView.setOnContextMenuRequested(event -> {
-      MenuItem select = new MenuItem("Select in explorer");
-      MenuItem combineTags = new MenuItem("Add tags to other -->");
-      ContextMenu cm = new ContextMenu(select, combineTags);
-      select.setOnAction(event1 -> {
-        if (selectListener != null) {
-          selectListener.pass(currentPair.getObject1());
-        }
-        cm.hide();
-        close();
-      });
-      combineTags.setOnAction(event1 -> {
-        currentPair.getObject1().getTags().forEach(tag -> currentPair.getObject2().addTag(tag));
-        cm.hide();
-      });
-      cm.show(leftMediaView.getScene().getWindow(), event.getScreenX(), event.getScreenY());
-    });
-    rightMediaView.setOnContextMenuRequested(event -> {
-      MenuItem select = new MenuItem("Select in explorer");
-      MenuItem combineTags = new MenuItem("<-- Add tags to other");
-      ContextMenu cm = new ContextMenu(select, combineTags);
-      select.setOnAction(event1 -> {
-        if (selectListener != null) {
-          selectListener.pass(currentPair.getObject2());
-        }
-        cm.hide();
-        close();
-      });
-      combineTags.setOnAction(event1 -> {
-        currentPair.getObject2().getTags().forEach(tag -> currentPair.getObject1().addTag(tag));
-        cm.hide();
-      });
-      cm.show(rightMediaView.getScene().getWindow(), event.getScreenX(), event.getScreenY());
-    });
-    leftTagList.setCellFactory(param -> {
-      // TODO
-      TagListCell c = new TagListCell(null, null) {
-        private final PseudoClass otherHasPseudoClass = PseudoClass.getPseudoClass("other-missing");
-
-        private final BooleanProperty otherMissing = new BooleanPropertyBase() {
-          @Override
-          protected void invalidated() {
-            pseudoClassStateChanged(otherHasPseudoClass, get());
-          }
-
-          @Override
-          public Object getBean() {
-            return this;
-          }
-
-          @Override
-          public String getName() {
-            return "otherMissing";
-          }
-        };
-
-        @Override
-        protected void updateItem(Tag tag, boolean empty) {
-          super.updateItem(tag, empty);
-
-          otherMissing.set(tag != null && !currentPair.getObject2().hasTag(tag));
-        }
-      };
-
-      MenuItem addToOther = new MenuItem("Add to other");
-      addToOther.setOnAction(event -> currentPair.getObject2().addTag(c.getItem()));
-      MenuItem removeTag = new MenuItem("Remove tag");
-      removeTag.setOnAction(event -> currentPair.getObject1().removeTag(c.getItem()));
-      ContextMenu cm = new ContextMenu(addToOther, new SeparatorMenuItem(), removeTag);
-      c.setOnContextMenuRequested(
-          event -> cm.show(c.getScene().getWindow(), event.getScreenX(), event.getScreenY()));
-      return c;
-    });
-    leftTagList.setPrefWidth(200);
-    rightTagList.setCellFactory(param -> {
-      // TODO
-      TagListCell c = new TagListCell(null, null) {
-        private final PseudoClass otherHasPseudoClass = PseudoClass.getPseudoClass("other-missing");
-
-        private final BooleanProperty otherMissing = new BooleanPropertyBase() {
-          @Override
-          protected void invalidated() {
-            pseudoClassStateChanged(otherHasPseudoClass, get());
-          }
-
-          @Override
-          public Object getBean() {
-            return this;
-          }
-
-          @Override
-          public String getName() {
-            return "otherMissing";
-          }
-        };
-
-        @Override
-        protected void updateItem(Tag tag, boolean empty) {
-          super.updateItem(tag, empty);
-
-          otherMissing.set(tag != null && !currentPair.getObject1().hasTag(tag));
-        }
-      };
-
-      MenuItem addToOther = new MenuItem("Add to other");
-      addToOther.setOnAction(event -> currentPair.getObject1().addTag(c.getItem()));
-      MenuItem removeTag = new MenuItem("Remove tag");
-      removeTag.setOnAction(event -> currentPair.getObject2().removeTag(c.getItem()));
-      ContextMenu cm = new ContextMenu(addToOther, new SeparatorMenuItem(), removeTag);
-      c.setOnContextMenuRequested(
-          event -> cm.show(c.getScene().getWindow(), event.getScreenX(), event.getScreenY()));
-      return c;
-    });
-    rightTagList.setPrefWidth(200);
-    leftInfoBox.setAlignment(Pos.BOTTOM_LEFT);
-    leftInfoBox.setMaxHeight(USE_PREF_SIZE);
-    leftInfoBox.setOpacity(0.75);
-    BorderPane.setAlignment(leftInfoBox, Pos.BOTTOM_LEFT);
-    rightInfoBox.setAlignment(Pos.BOTTOM_RIGHT);
-    rightInfoBox.setMaxHeight(USE_PREF_SIZE);
-    rightInfoBox.setOpacity(0.75);
-    BorderPane.setAlignment(rightInfoBox, Pos.BOTTOM_RIGHT);
-    BorderPane lbp = new BorderPane(null, null, leftTagList, null, leftInfoBox);
-    lbp.setPickOnBounds(false);
-    BorderPane rbp = new BorderPane(null, null, rightInfoBox, null, rightTagList);
-    rbp.setPickOnBounds(false);
-    SplitPane sp =
-        new SplitPane(new StackPane(leftMediaView, lbp), new StackPane(rightMediaView, rbp));
-    sp.setOnMouseEntered(event -> {
-      lbp.setRight(leftTagList);
-      rbp.setLeft(rightTagList);
-    });
-    sp.setOnMouseExited(event -> {
-      lbp.setRight(null);
-      rbp.setLeft(null);
-    });
-    setCenter(sp);
-
-    // ---------------------------------------- Bottom element -----------------------------------------------------
-    VBox bottom = new VBox(5);
-    bottom.setPadding(new Insets(5));
-    // Construct first element
-    indexTextField.setAlignment(Pos.CENTER_RIGHT);
-    indexTextField.setPrefWidth(50);
-    indexTextField.setOnAction(event -> {
-      int i = pairs.indexOf(currentPair);
-      try {
-        int temp = Integer.parseInt(indexTextField.getText()) - 1;
-        i = Math.max(0, Math.min(temp, pairs.size() - 1)); // Clamp to valid indices
-      } catch (NumberFormatException e) {
-        // Nothing
-      }
-
-      preview(pairs.get(i));
-      requestFocus();
-    });
-    HBox hbc = new HBox(indexTextField, similarityLabel);
-    hbc.setAlignment(Pos.CENTER);
-    Button leftDeleteButton = new Button("Delete");
-    leftDeleteButton.setOnAction(event -> deleteItem(currentPair.getObject1()));
-    Button rightDeleteButton = new Button("Delete");
-    rightDeleteButton.setOnAction(event -> deleteItem(currentPair.getObject2()));
-    HBox hbl = new HBox(leftDeleteButton);
-    hbl.setAlignment(Pos.CENTER_LEFT);
-    HBox hbr = new HBox(rightDeleteButton);
-    hbr.setAlignment(Pos.CENTER_RIGHT);
-    bottom.getChildren().add(new BorderPane(hbc, null, hbr, null, hbl));
-    // Construct second element
-    Button prevPairButton = new Button("<-");
-    prevPairButton.setOnAction(event -> previewPrev());
-    Button closeButton = new Button("Close");
-    closeButton.setOnAction(event -> close());
-    Button nextPairButton = new Button("->");
-    nextPairButton.setOnAction(event -> previewNext());
-    nonDupeCheckBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
-      if (newValue) {
-        menagerie.addNonDuplicate(currentPair);
-      } else {
-        menagerie.removeNonDuplicate(currentPair);
-      }
-    });
-    HBox hb = new HBox(5, prevPairButton, closeButton, nextPairButton, nonDupeCheckBox);
-    hb.setAlignment(Pos.CENTER);
-    bottom.getChildren().add(hb);
-    setBottom(bottom);
-
-
-    setDefaultFocusNode(closeButton);
   }
 
   /**
@@ -398,36 +379,44 @@ public class DuplicatesScreen extends Screen {
     updatePreload(pair);
 
     if (pair != null) {
-      leftMediaView.preview(pair.getObject1());
-      rightMediaView.preview(pair.getObject2());
-
-      leftTagList.getItems().addAll(pair.getObject1().getTags());
-      leftTagList.getItems().sort(Comparator.comparing(Tag::getName));
-      currentPair.getObject1().getTags().addListener(leftTagListener);
-
-      rightTagList.getItems().addAll(pair.getObject2().getTags());
-      rightTagList.getItems().sort(Comparator.comparing(Tag::getName));
-      currentPair.getObject2().getTags().addListener(rightTagListener);
-
-      leftInfoBox.setItem(pair.getObject1());
-      rightInfoBox.setItem(pair.getObject2());
-
-      DecimalFormat df = new DecimalFormat("#.##");
-      indexTextField.setText((pairs.indexOf(pair) + 1) + "");
-      similarityLabel.setText(
-          "/" + pairs.size() + ": " + df.format(pair.getSimilarity() * 100) + "%");
-
-      nonDupeCheckBox.setSelected(menagerie.hasNonDuplicate(pair));
+      previewPair(pair);
     } else {
-      leftMediaView.preview(null);
-      rightMediaView.preview(null);
-
-      leftInfoBox.setItem(null);
-      rightInfoBox.setItem(null);
-
-      similarityLabel.setText("N/A");
-      nonDupeCheckBox.setSelected(false);
+      resetPreview();
     }
+  }
+
+  private void resetPreview() {
+    leftMediaView.preview(null);
+    rightMediaView.preview(null);
+
+    leftInfoBox.setItem(null);
+    rightInfoBox.setItem(null);
+
+    similarityLabel.setText("N/A");
+    nonDupeCheckBox.setSelected(false);
+  }
+
+  private void previewPair(SimilarPair<MediaItem> pair) {
+    leftMediaView.preview(pair.getObject1());
+    rightMediaView.preview(pair.getObject2());
+
+    leftTagList.getItems().addAll(pair.getObject1().getTags());
+    leftTagList.getItems().sort(Comparator.comparing(Tag::getName));
+    currentPair.getObject1().getTags().addListener(leftTagListener);
+
+    rightTagList.getItems().addAll(pair.getObject2().getTags());
+    rightTagList.getItems().sort(Comparator.comparing(Tag::getName));
+    currentPair.getObject2().getTags().addListener(rightTagListener);
+
+    leftInfoBox.setItem(pair.getObject1());
+    rightInfoBox.setItem(pair.getObject2());
+
+    DecimalFormat df = new DecimalFormat("#.##");
+    indexTextField.setText((pairs.indexOf(pair) + 1) + "");
+    similarityLabel.setText(
+        "/" + pairs.size() + ": " + df.format(pair.getSimilarity() * 100) + "%");
+
+    nonDupeCheckBox.setSelected(menagerie.hasNonDuplicate(pair));
   }
 
   private void updatePreload(SimilarPair<MediaItem> pair) {
@@ -475,7 +464,7 @@ public class DuplicatesScreen extends Screen {
     }
 
     //Remove other pairs containing the deleted image
-    for (SimilarPair pair : new ArrayList<>(pairs)) {
+    for (final var pair : new ArrayList<>(pairs)) {
       if (toDelete.equals(pair.getObject1()) || toDelete.equals(pair.getObject2())) {
         int i = pairs.indexOf(pair);
         pairs.remove(pair);
