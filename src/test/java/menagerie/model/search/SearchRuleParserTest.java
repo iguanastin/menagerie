986901e4f44c43ac927f1d9b51d0d19def8067c2
Elias <elias.santner@edu.htl-villach.at>
2023-01-04 12:48:55 +0100

    minor improvements to test code quality
    - split up long test cases
    - use parameterized tests

--- src/test/java/menagerie/model/search/SearchRuleParserTest.java
+++ src/test/java/menagerie/model/search/SearchRuleParserTest.java
@@ -5,60 +5,54 @@ import menagerie.model.search.rules.IDRule;
 import menagerie.model.search.rules.SearchRule;
 import menagerie.model.search.rules.TitleRule;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SearchRuleParserTest {
 
-  @Test
-  void testParseIDRule() {
-    // Test parsing ID rule with equal to operator
-    List<SearchRule> rules = SearchRuleParser.parseRules("id:123");
-    assertEquals(1, rules.size());
-    assertTrue(rules.get(0) instanceof IDRule);
-    assertEquals(123, ((IDRule) rules.get(0)).getId());
-    assertEquals(IDRule.Type.EQUAL_TO, ((IDRule) rules.get(0)).getType());
 
-    // Test parsing ID rule with less than operator
-    rules = SearchRuleParser.parseRules("id:<123");
+  @ParameterizedTest
+  @MethodSource("idRuleToRange")
+  void testParseIDRule(String ruleString, IDRule.Type type) {
+    List<SearchRule> rules = SearchRuleParser.parseRules(ruleString);
     assertEquals(1, rules.size());
     assertTrue(rules.get(0) instanceof IDRule);
     assertEquals(123, ((IDRule) rules.get(0)).getId());
-    assertEquals(IDRule.Type.LESS_THAN, ((IDRule) rules.get(0)).getType());
-
-    // Test parsing ID rule with greater than operator
-    rules = SearchRuleParser.parseRules("id:>123");
-    assertEquals(1, rules.size());
-    assertTrue(rules.get(0) instanceof IDRule);
-    assertEquals(123, ((IDRule) rules.get(0)).getId());
-    assertEquals(IDRule.Type.GREATER_THAN, ((IDRule) rules.get(0)).getType());
+    assertEquals(type, ((IDRule) rules.get(0)).getType());
   }
 
-  @Test
-  void testParseDateRule() {
-    // Test parsing date rule with equal to operator
-    List<SearchRule> rules = SearchRuleParser.parseRules("date:1671647540865");
-    assertEquals(1, rules.size());
-    assertTrue(rules.get(0) instanceof DateAddedRule);
-    assertEquals(1671647540865L, ((DateAddedRule) rules.get(0)).getTime());
-    assertEquals(DateAddedRule.Type.EQUAL_TO, ((DateAddedRule) rules.get(0)).getType());
+  private static Stream<Arguments> idRuleToRange() {
+    return Stream.of(
+        arguments("id:123", IDRule.Type.EQUAL_TO),
+        arguments("id:<123", IDRule.Type.LESS_THAN),
+        arguments("id:>123", IDRule.Type.GREATER_THAN)
+    );
+  }
 
-    // Test parsing date rule with less than operator
-    rules = SearchRuleParser.parseRules("date:<1671647540865");
+  @ParameterizedTest
+  @MethodSource("dateRuleToRange")
+  void testParseDateRule(String ruleString, DateAddedRule.Type type) {
+    List<SearchRule> rules = SearchRuleParser.parseRules(ruleString);
     assertEquals(1, rules.size());
     assertTrue(rules.get(0) instanceof DateAddedRule);
     assertEquals(1671647540865L, ((DateAddedRule) rules.get(0)).getTime());
-    assertEquals(DateAddedRule.Type.LESS_THAN, ((DateAddedRule) rules.get(0)).getType());
+    assertEquals(type, ((DateAddedRule) rules.get(0)).getType());
+  }
 
-    // Test parsing date rule with greater than operator
-    rules = SearchRuleParser.parseRules("date:>1671647540865");
-    assertEquals(1, rules.size());
-    assertTrue(rules.get(0) instanceof DateAddedRule);
-    assertEquals(1671647540865L, ((DateAddedRule) rules.get(0)).getTime());
-    assertEquals(DateAddedRule.Type.GREATER_THAN, ((DateAddedRule) rules.get(0)).getType());
+  private static Stream<Arguments> dateRuleToRange() {
+    return Stream.of(
+        arguments("date:1671647540865", DateAddedRule.Type.EQUAL_TO),
+        arguments("date:<1671647540865", DateAddedRule.Type.LESS_THAN),
+        arguments("date:>1671647540865", DateAddedRule.Type.GREATER_THAN)
+    );
   }
 
   @Test
--- src/test/java/menagerie/model/search/rules/TitleRuleTest.java
+++ src/test/java/menagerie/model/search/rules/TitleRuleTest.java
@@ -12,33 +12,42 @@ class TitleRuleTest {
     return new GroupItem(null, 1, 1, title);
   }
 
+  private final GroupItem item1 = createGroupItem("Title with search word");
+  private final GroupItem item2 = createGroupItem("Title without looked-for terms");
+  private final GroupItem item3 = createGroupItem("Title with Search WORD (different case)");
+  private final GroupItem item4 = createGroupItem("Title with search word and extra words");
+
+  private final TitleRule rule1 = new TitleRule("search word", false);
+  private final TitleRule rule2 = new TitleRule("search word", true);
+  private final TitleRule rule3 = new TitleRule("Search WORD", false);
+  private final TitleRule rule4 = new TitleRule("extra words", false);
+
   @Test
-  void testAccept() {
-    GroupItem item1 = createGroupItem("Title with search word");
-    GroupItem item2 = createGroupItem("Title without looked-for terms");
-    GroupItem item3 = createGroupItem("Title with Search WORD (different case)");
-    GroupItem item4 = createGroupItem("Title with search word and extra words");
-
-    TitleRule rule1 = new TitleRule("search word", false);
-    TitleRule rule2 = new TitleRule("search word", true);
-    TitleRule rule3 = new TitleRule("Search WORD", false);
-    TitleRule rule4 = new TitleRule("extra words", false);
-
+  void testAcceptRule1() {
     assertTrue(rule1.accept(item1));
     assertFalse(rule1.accept(item2));
     assertTrue(rule1.accept(item3));
     assertTrue(rule1.accept(item4));
+  }
 
+  @Test
+  void testAcceptRule2() {
     assertFalse(rule2.accept(item1));
     assertTrue(rule2.accept(item2));
     assertFalse(rule2.accept(item3));
     assertFalse(rule2.accept(item4));
+  }
 
+  @Test
+  void testAcceptRule3() {
     assertTrue(rule3.accept(item1));
     assertFalse(rule3.accept(item2));
     assertTrue(rule3.accept(item3));
     assertTrue(rule3.accept(item4));
+  }
 
+  @Test
+  void testAcceptRule4() {
     assertFalse(rule4.accept(item1));
     assertFalse(rule4.accept(item2));
     assertFalse(rule4.accept(item3));
